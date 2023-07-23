2d98642d666a9c4f542293d100400b9aa68b0fc1
karimliaghabala <agabala.93@gmail.com>
2023-07-24 01:45:14 +0400

    added json file

--- script.js
+++ script.js
@@ -5,15 +5,40 @@ let email = document.querySelector("#email");
 let btn = document.querySelector("#btn");
 let tbody = document.querySelector("tbody");
 
+let firstTd = document.querySelector("#firstTd");
+let lastTd = document.querySelector("#lastTd");
+let emailTd = document.querySelector("#emailTd");
+let deleteTd = document.querySelector("#deleteTd>button");
+
+
+
+fetch('data.json')
+.then((res) => {
+    return res.json()
+})
+.then((data) => {
+    data.forEach(e => {
+        firstTd.textContent = e.firstName
+        lastTd.textContent = e.lastName
+        emailTd.textContent = e.email
+    })
+    deleteTd.addEventListener("click", (i) => {
+        delTrjson = i.target.parentElement.parentElement.firstChild.textContent;
+        tdDelete = i.target.parentElement.parentElement;
+        data.filter(item => item.firstTd != delTrjson)
+        tdDelete.remove()
+    })
+})
+
 let userList = [];
-function Users (firstName,lastName,email){
+function Users(firstName, lastName, email) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
 }
-btn.addEventListener("click", (e)=>{
+btn.addEventListener("click", (e) => {
     e.preventDefault();
-    newUser = new Users(firstName.value,lastName.value,email.value);
+    newUser = new Users(firstName.value, lastName.value, email.value);
     userList.push(newUser);
 
     tr = document.createElement("tr");
@@ -23,7 +48,6 @@ btn.addEventListener("click", (e)=>{
     trLast = document.createElement("td");
     trMail = document.createElement("td");
     tdBtns = document.createElement("td");
-    
 
     tr.append(trFirst);
     tr.append(trLast);
@@ -35,16 +59,15 @@ btn.addEventListener("click", (e)=>{
 
     btnDelete.textContent = "Delete";
 
-    userList.forEach(item=>{
+    userList.forEach(item => {
         trFirst.textContent = item.firstName
         trLast.textContent = item.lastName
         trMail.textContent = item.email
     })
-
-    btnDelete.addEventListener("click",(e)=>{
+    btnDelete.addEventListener("click", (e) => {
         delTr = e.target.parentElement.parentElement.firstChild.textContent;
         trDeleted = e.target.parentElement.parentElement
-        userList = userList.filter(item=>item.trFirst != delTr )
+        userList = userList.filter(item => item.trFirst != delTr)
         trDeleted.remove()
     })
 })
